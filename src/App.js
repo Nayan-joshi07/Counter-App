f8af659df1ecc4e990d3dfb95eb44d6ec62c2512
nayanjoshi <nayan@bombayplay.com>
2022-05-26 09:41:28 +0530

    Improved Layout

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
+
 function App() {
-  const [count, setCount] = useState(5);
-  return (
-    <div className="container my-5">
-      <div className="cardn text-center my-5">
-        <div className="card-body">
-          <h1> Counter App</h1>
-          <div className="my-5">
-            <h2 className="my-5">{count}</h2>
-            <button className="btn btn-success mx-3" onClick={() => setCount(count + 1)}>Increment</button>
-            <button className="btn btn-danger mx-3" onClick={() => setCount(count - 1)} disabled={count === 0}>Decrement</button>
-            <button className="btn btn-secondary mx-3" onClick={() => setCount(0)} disabled={count === 0}>Reset</button>
-          </div>
+    const [count, setCount] = useState(0);
+    function handleButtonClick() {
+        setCount(count + 1);
+    }
+
+    return (
+        <div className="container-body">
+            <div class="form-header">
+                <h4 class="title">Counter App</h4>
+            </div>
+            <h1 class="title-text">{count}</h1>
+            <button class="proceed-btn" onClick={function () { setCount(count + 1) }}>Increment</button>
+            <button class="proceed-btn" onClick={function () { setCount(count - 1) }} disabled={count === 0}>Decrement</button>
+            <button class="proceed-btn" onClick={function () { setCount(0) }} disabled={count === 0}>Reset</button>
         </div>
-      </div>
-    </div>);
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import AppTwo from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AppTwo />
   </React.StrictMode>,
   document.getElementById('root')
+
 );
